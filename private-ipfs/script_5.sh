75277dcff25c9a14ffc75b984de4ad5e6a5ac013
Alx-ea <alex.emer.arf@gmail.com>
2024-05-11 15:28:41 +0100

    unnecessary files

--- private-ipfs/script_5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "hi"
-for n_nodes in {1..3}; do
-    echo "Running script with n_nodes=$n_nodes..."
-    sed -i "s/n_nodes=11/n_nodes=$n_nodes/" script_1_build_images_create_containers.sh
-    bash script_1_build_images_create_containers.sh
-done
